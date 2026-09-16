19493062a5639875989f6e1884efa91f9fd82903
Aya Ahmed <ayaahmed.com2006@gmail.com>
2026-09-16 16:27:33 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,10 +2,14 @@ const app = require("./app");
 require("dotenv").config();
 const connectDB = require("./config/db");
 
-const PORT = process.env.PORT || 3000;
-
 connectDB();
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+
+module.exports = app;
