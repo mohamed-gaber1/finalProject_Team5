cbe5b1c5cf83fbffb2f287d94ed8c45947cf47a4
Aya Ahmed <ayaahmed.com2006@gmail.com>
2026-09-16 16:39:47 +0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URL, {
+      serverSelectionTimeoutMS: 5000,
+    });
     console.log('MongoDB Connected Successfully!');
   } catch (error) {
     console.error('Database connection failed:', error.message);
-    process.exit(1);
+ 
   }
 };
 
