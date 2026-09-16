e021c27c0177935a69f8f0df2cbf338a2b17a855
mohamed-gaber1 <m.gaber.fouda123@gmail.com>
2026-09-16 18:49:00 +0300

    Simplify CORS configuration in app.js

--- src/app.js
+++ src/app.js
@@ -3,16 +3,7 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://finalproject-team5.vercel.app",
-      "https://deep-dive-final-project.vercel.app",
-    ],
-    credentials: true,
-  } )
-);
+app.use(cors());
 
 app.use(express.json());
 
